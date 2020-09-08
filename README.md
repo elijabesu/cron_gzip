ca116a3111d1d17659fe2e4d0b93984ba8a1ed55
elijabesu <ellie@saurich.com>
2020-09-08 08:58:09 +0200

    Verze bez data

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # cron_gzip
 
-Tento projekt vezme veškeré `.log` soubory ze složky `/var/log` a zazipuje je do `.gz` souborů, poté původní soubory vyčistí (promaže) do nulového stavu.
+Tento skript vezme veškeré `.log` soubory ze složky `/var/log` a zazipuje je do `.gz` souborů, poté původní soubory vyčistí (promaže) do nulového stavu.
+
+Jsou tu zde dvě různé verze podle toho, co přesně od skriptu potřebujete:
+- `gzip_files.py` po každém spuštění nahradí původní soubory
+- `gzip_files_date.py` přidá datum spuštění k názvu `.gz` souboru a při spuštění jiný den vytvoří nový `.gz` soubor.
 
 ## Příklad použití
+Konkrétně je zde ukázaný `gzip_files_date.py`.
+
 Máme následující soubory ve složce `/var/log`:
 
 ![pred](https://github.com/elijabesu/cron_gzip/blob/master/pictures/pred-spustenim.png)
--- gzip_files.py
+++ gzip_files.py
@@ -8,11 +8,10 @@ def main():
         if not file.endswith(".log"):
             continue
 
-        file_name = split_file_name(file)
-        paths = get_paths(path, file_name)
+        paths = get_paths(path, file)
 
         with open(paths[0], "r") as f:
-            with gzip.open(paths[1], "wt+") as fgz:
+            with gzip.open(paths[1], "wt") as fgz:
                 fgz.writelines(f)
 
         clear_file(paths[0])
@@ -20,16 +19,10 @@ def main():
 def clear_file(path):
     open(path, "w").close()
 
-def split_file_name(file):
-    file_name = list()
-    file_name = file.split(".")
-    return file_name
-
-def get_paths(path, file_name):
+def get_paths(path, file):
     paths = list()
-    path_file_name = [path + "/" + file_name[0], "." + file_name[1]]
-    paths.append(path_file_name[0] + path_file_name[1])
-    paths.append(path_file_name[0] + "_" + str(datetime.date.today()) + path_file_name[1] + ".gz")
+    paths.append(path + "/" + file)
+    paths.append(path + "/" + file + ".gz")
     return paths
 
 if __name__ == "__main__":
